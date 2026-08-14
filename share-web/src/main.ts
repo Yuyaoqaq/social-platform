e9e0e84c642f5b0969be9f10c04f48d295221738
Chengyuyao <2170182562@qq.com>
2026-08-14 18:21:02 +0800

    Delete share-web directory

--- share-web/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import 'vant/lib/index.css'
-import '@/assets/styles/main.scss'
-import App from '@/App.vue'
-import router from '@/router'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
